1430e1ba04797a165da0fb16f94ae2e2c5e8a353
jarif12538 <hasanjarif12538@gmail.com>
2026-02-06 21:37:47 +0600

    Add rosette drawing functionality using turtle graphics

--- round.py
+++ round.py
@@ -18,3 +18,20 @@ palette = [
     (70, 58, 44),   # walnut
 ]
 
+for index in range(num):
+    color(palette[index % len(palette)])
+    for arm in range(4):
+        goto(0, 0)
+        setheading(ang * index + (arm * 90))
+        forward(180)
+        left(90)
+        arc = 120
+        forward(arc)
+        left(45)
+        forward(40)
+        left(90)
+        forward(40)
+        left(45)
+        forward(arc)
+       
+done()
