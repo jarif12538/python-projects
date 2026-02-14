f2e7d030e07262d6395ea650a68f8c607605b6ef
jarif12538 <hasanjarif12538@gmail.com>
2026-02-14 21:49:34 +0600

    Enhance Guess the Number game with attempt count and improved prompts

--- guess_the_number.py
+++ guess_the_number.py
@@ -3,9 +3,12 @@ lowest = 1
 highest = 100
 answer = random.randint(lowest, highest)
 is_correct = False
+count = 0
+print(*"******************************")
 print("Welcome to the Guess the Number Game!")
-print("Try Your Luck!")
+print("want to Try Your Luck! today?")
 print(f"I'm thinking of a number between {lowest} and {highest}. Can you guess it?")
+print(*"******************************")
 while not is_correct:
     user_input = input("Enter your Number: ").strip()
     if not user_input.isdigit():
@@ -15,6 +18,7 @@ while not is_correct:
     if guess < lowest or guess > highest:
         print(f"Please enter a number between {lowest} and {highest}.")
         continue
+    count += 1
     if guess < answer:
         print("Too low! Try again.")
     elif guess > answer:
@@ -23,5 +27,6 @@ while not is_correct:
         print(("******************************"))
         print(f"Congratulations! You've guessed the number correctly!")
         print(f"The correct number was: {answer}")
+        print(f"It took you {count} attempts to guess the number.")
         print(("******************************"))
         is_correct = True
