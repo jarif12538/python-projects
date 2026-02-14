bd4706655a4411a91af0b1cb8c5962c76feb7b1c
jarif12538 <hasanjarif12538@gmail.com>
2026-02-14 21:43:05 +0600

    Refactor input prompt for clarity in Guess the Number game

--- guess_the_number.py
+++ guess_the_number.py
@@ -1,27 +1,20 @@
 import random
-
 lowest = 1
 highest = 100
 answer = random.randint(lowest, highest)
 is_correct = False
-
 print("Welcome to the Guess the Number Game!")
 print("Try Your Luck!")
 print(f"I'm thinking of a number between {lowest} and {highest}. Can you guess it?")
-
 while not is_correct:
-    user_input = input("Enter your guess: ").strip()
-
+    user_input = input("Enter your Number: ").strip()
     if not user_input.isdigit():
         print("Invalid input. Please enter a valid integer.")
         continue
-
     guess = int(user_input)
-
     if guess < lowest or guess > highest:
         print(f"Please enter a number between {lowest} and {highest}.")
         continue
-
     if guess < answer:
         print("Too low! Try again.")
     elif guess > answer:
