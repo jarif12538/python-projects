1fa9060908fedbf3e0c9a78bc26599db31ecc32c
jarif12538 <hasanjarif12538@gmail.com>
2026-02-14 21:39:48 +0600

    Add Guess the Number game implementation

---
+++ guess_the_number.py
@@ -0,0 +1,34 @@
+import random
+
+lowest = 1
+highest = 100
+answer = random.randint(lowest, highest)
+is_correct = False
+
+print("Welcome to the Guess the Number Game!")
+print("Try Your Luck!")
+print(f"I'm thinking of a number between {lowest} and {highest}. Can you guess it?")
+
+while not is_correct:
+    user_input = input("Enter your guess: ").strip()
+
+    if not user_input.isdigit():
+        print("Invalid input. Please enter a valid integer.")
+        continue
+
+    guess = int(user_input)
+
+    if guess < lowest or guess > highest:
+        print(f"Please enter a number between {lowest} and {highest}.")
+        continue
+
+    if guess < answer:
+        print("Too low! Try again.")
+    elif guess > answer:
+        print("Too high! Try again.")
+    else:
+        print(("******************************"))
+        print(f"Congratulations! You've guessed the number correctly!")
+        print(f"The correct number was: {answer}")
+        print(("******************************"))
+        is_correct = True
